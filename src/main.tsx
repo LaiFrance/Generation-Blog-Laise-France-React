41b45ffa7d2fa8ffa6380140effa4ac34bb28a4b
Laise France <laise94@hotmail.com>
2023-05-06 03:01:01 -0300

    service

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+
 ReactDOM.render(
-<React.StrictMode>
-<App />
-</React.StrictMode>,
-document.getElementById('root') as HTMLElement
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('#root') 
 );
