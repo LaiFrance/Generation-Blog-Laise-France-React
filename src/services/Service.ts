104f724e0730420530b8013bfd74251b5a928220
Laise France <laise94@hotmail.com>
2023-05-06 02:28:03 -0300

    service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://b-blog-laise-france.onrender.com/'
+    baseURL: 'localhost:5000',
 })
 
     export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
